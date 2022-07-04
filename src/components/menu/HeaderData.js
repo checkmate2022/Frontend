1c86f637b54fb81dabdc3e719745b78bb3235d5a
yunva17 <jiyoon40117@gmail.com>
2022-07-04 23:59:38 +0900

    [Feat]  헤더

---
+++ src/components/menu/HeaderData.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const HeaderData = [
+  {
+    id: 1,
+    title: '메인',
+    path: '/',
+    type: 'main',
+  },
+  {
+    id: 2,
+    title: '프로젝트',
+    path: '/team',
+    type: 'teammain',
+  },
+  {
+    id: 3,
+    title: '마이페이지',
+    path: '/mypage',
+    type: 'mypage',
+  },
+];
