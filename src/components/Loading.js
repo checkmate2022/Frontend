14fa96b6a997527bf1b9bed4a967c3c3b4a129b1
yunva17 <jiyoon40117@gmail.com>
2022-08-04 14:47:51 +0900

    [feat] 로딩 스피너

---
+++ src/components/Loading.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import spinner from '../styles/icons/spinner2.gif';
+
+const Background = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.div`
+  text-align: center;
+`;
+
+function Loading() {
+  return (
+    <Background>
+      <img src={spinner} width='6%' alt='로딩중' />
+      <LoadingText>로딩중..</LoadingText>
+    </Background>
+  );
+}
+
+export default Loading;
