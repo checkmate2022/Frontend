fd75793509ecfdf9d8335ae29b3f3f6e65f34ec9
yunva17 <jiyoon40117@gmail.com>
2022-08-22 06:15:03 +0900

    [feat] 댓글 등록, 수정, 삭제

---
+++ src/components/team/CommentList.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { colors } from '../../styles/theme';
+import moment from 'moment';
+import {
+  onCommentChange,
+  onCommentDelete,
+  onCommentGet,
+} from '../../api/teamboard';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { commentState } from '../../store/boardstore';
+import CommentAdd from './CommentAdd';
+
+const NameText = styled.span`
+  font-size: ${({ size }) => (size ? size : 14)}px;
+  margin-left: ${({ bottom }) => (bottom ? bottom : 15)}px;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: fit-content;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const StyledImage = styled.img`
+  border-radius: 10px;
+  width: 38px;
+  height: 38px;
+`;
+
+const Line = styled.hr`
+  width: 100%;
+  height: 0px;
+  border: 0.5px solid ${colors.grey3};
+  margin: 10px 0 20px 0;
+`;
+
+const EditContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+`;
+
+const EditText = styled.span`
+  font-size: 12px;
+  cursor: pointer;
+  opacity: 0.8;
+  margin-left: 8px;
+`;
+
+function CommentList({ comment, boardId, onEdit, isEdit, resetIdx, userName }) {
+  const [commentList, setCommentList] = useRecoilState(commentState);
+  const [content, setContent] = useState(comment.content);
+
+  // 댓글 수정
+  const onCommentEditButton = (event) => {
+    event.preventDefault();
+    onCommentChange(comment.commentSeq, content).then((result) => {
+      if (result) {
+        onCommentGet(boardId).then((res) => setCommentList(res));
+        resetIdx();
+      }
+    });
+  };
+
+  // 댓글 삭제
+  const onCommentDeleteButton = (commentId) => {
+    onCommentDelete(commentId).then((result) => {
+      if (result) {
+        onCommentGet(boardId).then((res) => setCommentList(res));
+      }
+    });
+  };
+  return (
+    <div>
+      {isEdit ? (
+        <>
+          <CommentAdd
+            onSubmit={onCommentEditButton}
+            userImage={comment.userImage}
+            content={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          <EditContainer>
+            <EditText onClick={resetIdx}>취소</EditText>
+          </EditContainer>
+        </>
+      ) : (
+        <>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <StyledImage src={comment.userImage} />
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <ProfileContainer>
+                <NameText>{comment.username}</NameText>
+                <NameText size={11} bottom={5}>
+                  {moment(comment.modifiedDate).format('YYYY/MM/DD  HH:mm')}
+                </NameText>
+              </ProfileContainer>
+              <NameText size={15}>{comment.content}</NameText>
+            </div>
+          </div>
+          {userName === comment.username ? (
+            <EditContainer>
+              <EditText onClick={onEdit}>수정</EditText>
+              <EditText
+                onClick={() => onCommentDeleteButton(comment.commentSeq)}
+              >
+                삭제
+              </EditText>
+            </EditContainer>
+          ) : null}
+        </>
+      )}
+      <Line />
+    </div>
+  );
+}
+
+export default CommentList;
